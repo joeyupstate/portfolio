9949d13e1c5e15e6fa9ef460b6a67f6d2a09feb5
Joe Abele <josephabele2@gmail.com>
2021-03-01 15:14:13 -0500

    added an x icon to close menu

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const nav = document.querySelector(".nav");
 const visbleNav = document.querySelector(".visible-nav")
 const hamburgerButton = document.querySelector(".hamburger-icon")
+const closeIcon = document.querySelector(".close-icon")
 const nameTitle = document.querySelector(".name-title")
 
 
@@ -8,5 +9,12 @@ hamburgerButton.addEventListener("click", function(){
 
 nav.classList.toggle("visible-nav")
 nameTitle.classList.toggle("name-title-blur")
+closeIcon.style.display="block"
 })
 
+
+closeIcon.addEventListener("click", function(){
+    nav.classList.toggle("visible-nav")
+    nameTitle.classList.toggle("name-title-blur")
+    closeIcon.style.display ="none"
+})
