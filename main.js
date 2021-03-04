c2bdc002be21f789f2c5ed4b156a6a453cf465f7
Joe Abele <josephabele2@gmail.com>
2021-03-04 09:38:30 -0500

    added animatiosn to buttons

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ const visbleNav = document.querySelector(".visible-nav")
 const hamburgerButton = document.querySelector(".hamburger-icon")
 const closeIcon = document.querySelector(".close-icon")
 const nameTitle = document.querySelector(".name-title")
+const viewWorkButton = document.querySelector("#view-work-button")
+const arrowIcon= document.querySelector(".arrow-icon")
+const arrowIconRotated= document.querySelector(".arrow-icon-rotated")
 
 
 hamburgerButton.addEventListener("click", function(){
@@ -17,4 +20,12 @@ closeIcon.addEventListener("click", function(){
     nav.classList.toggle("visible-nav")
     nameTitle.classList.toggle("name-title-blur")
     closeIcon.style.display ="none"
+})
+
+viewWorkButton.addEventListener("mouseenter", function(){
+    arrowIcon.style.transform="rotate(90deg)"
+})
+
+viewWorkButton.addEventListener("mouseleave", function(){
+    arrowIcon.style.transform="none"
 })
